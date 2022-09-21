716bf22bbc526ae84106b0be08ffc9df8e3af014
Natasha Gough <natash.gough37@gmail.com>
2022-09-21 13:59:09 -0500

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,13 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    int year = 1988;
+    String month = "January";
+    boolean a = true;
+    boolean b = false;
+    System.out.println(year);
+    System.out.println(month);
+    System.out.println(a);
+
+
+  }
+}
