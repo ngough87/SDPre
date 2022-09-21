6e205a09588507efeaa510b3807a35fe5e097855
Natasha Gough <natash.gough37@gmail.com>
2022-09-21 13:48:59 -0500

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Natasha Gough";
+    int age =35;
+    String state = "California";
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(state);
+  }
+}
