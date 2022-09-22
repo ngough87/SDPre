a3ca7f9f499bc9765192a32bba3e42f7f129a25c
Natasha Gough <natash.gough37@gmail.com>
2022-09-22 09:10:18 -0500

    complete objective3lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,10 @@
+public class Objective3Lab2 {
+  public static void main(String [] args) {
+  double num1 = Math.pow(3,2);
+  double num2 = Math.pow(2,2);
+  double hypotenuse = num1 + num2;
+  double result = Math.sqrt(hypotenuse);
+  String result1 = "The hypotenuse of a trianlge with sides 3.0 and 2.0 is " + result;
+  System.out.println(result1);
+  }
+}
