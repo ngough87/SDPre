20341f02dcee20f718479a0fdb8340376e793d9c
Natasha Gough <natash.gough37@gmail.com>
2022-09-22 08:15:55 -0500

    complete objective3lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,9 @@
+public class Objective3Lab1 {
+  public static void main(String [] args) {
+    int num1 = 15;
+    int num2 = 25;
+    int num3 = 147;
+    int average = (num1+num2+num3)/3;
+    System.out.println(average);
+  }
+}
