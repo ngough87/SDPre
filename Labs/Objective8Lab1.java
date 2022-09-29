6ca300485202ab6f21cf674b5d65c77c0f620588
Natasha Gough <natash.gough37@gmail.com>
2022-09-29 13:43:37 -0500

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,11 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+
+    for (int i=1; i<=10; i+=1){
+      System.out.println(i);
+    }
+    for (int ii=10; ii>=1; ii-=1){
+      System.out.println(ii);
+    }
+  }
+}
