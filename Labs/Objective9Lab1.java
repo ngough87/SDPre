9bf59a57e752874f9e01f768da80722ac4826880
Natasha Gough <natash.gough37@gmail.com>
2022-10-05 09:40:14 -0500

    complete objective9lab1

---
+++ Labs/Objective9Lab1.java
@@ -0,0 +1,9 @@
+public class Objective9Lab1 {
+  public static void main(String[] args) {
+    printHelloDoug();
+  }
+
+  public static void printHelloDoug() {
+      System.out.println("Hello Doug!");
+  }
+}
